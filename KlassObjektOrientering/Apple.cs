9ff9cf3559f01a7b81ca6e463f75945b94fe4951
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2023-05-15 14:57:45 +0200

    done

---
+++ KlassObjektOrientering/Apple.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+public class Apple
+{
+    public int cost = 50;
+    public string typeOfApple = "Granny Smith";
+    
+}
---
+++ KlassObjektOrientering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+Shopper shopper = new Shopper();
+Apple apple = new Apple();
+
+
+
+Console.WriteLine($"you buy a {apple.typeOfApple} for {apple.cost} dollars. ");
+Console.ReadLine();
+
